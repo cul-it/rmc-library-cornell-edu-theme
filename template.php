2263a5e1e3bad880fd79271d86c29f5745732d0d
Ken Williams <kaw5@cornell.edu>
2015-07-31 23:15:37 -0400

    Update template.php

--- template.php
+++ template.php
@@ -8,6 +8,6 @@
 /**
  * Overrides theme_menu_link().
  */
-function rmc_library_bootstrap_menu_link__menu_block($variables) {
+function rmc-library-cornell-edu-theme_menu_link__menu_block($variables) {
   return theme_menu_link($variables);
 }
